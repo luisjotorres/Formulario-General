148b6851fcbf5ce3b5313784da151ce695dd24e5
hardevkoder <haroldvaldes@yahoo.com>
2023-01-25 04:34:38 -0500

    Se Agregan Sweets Alerts

---
+++ js/script.js
@@ -0,0 +1,81 @@
+window.addEventListener('load',function(e){
+
+  const boton = document.getElementById('formulario');
+
+  boton.addEventListener('submit', function(e){
+    e.preventDefault();
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const edad = document.getElementById('edad').value;
+
+    if(nombre === '' || apellido ==='' || edad === '' ){
+      Swal.fire({
+        position: 'center',
+        icon: 'error',
+        title: 'Faltan datos',
+        showConfirmButton: false,
+        timer: 2000
+      })
+    }else{
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: `${nombre} ${apellido}`,
+        text:`Tienes ${edad} Años de Edad`,
+        showConfirmButton: false,
+        timer: 3000
+      })
+    }
+  });
+})
+
+
+
+
+
+
+
+
+
+
+// window.addEventListener("load", () => {
+
+//   const btnEnviar = document.getElementById("formulario");
+//   const txtNombre = document.getElementById("nombre");
+//   const txtApellido = document.getElementById("apellido");
+//   const txtEdad = document.getElementById("edad");
+
+//   // Funcion Para Generar Mensajes SweetAlert;
+//   const mostrarSweetAlert = (icon, title, footer, time) => {
+//     Swal.fire({
+//       position: "center",
+//       icon: icon,
+//       title: title,
+//       showConfirmButton: false,
+//       text: footer,
+//       timer: time,
+//       width:300,
+//       heightAuto:false,
+//       background:'#B1C9F6'
+//     });
+//   }
+
+
+  
+
+//   btnEnviar.addEventListener('submit',()=>{
+//     // mostrarSweetAlert("success", "Felicidades", "Datos Enviados Correctamente!", 12000);
+//     Swal.fire('Any fool can use a computer')
+
+
+//   })
+
+//   // document.getElementById("btnEnviar").addEventListener("click", function(){
+//   //   swal("Mensaje de alerta", {
+//   //     buttons: false,
+//   //     timer: 2000,
+//   //   });
+//   // });
+
+
+// });
