cb3345f64a56dfb261af9c7a233d2cfae14e799f
hardevkoder <haroldvaldes@yahoo.com>
2023-01-26 16:59:12 -0500

    Se Crea Funcion para configurar mensaje y evitar repetir codigo (DRY)

--- js/script.js
+++ js/script.js
@@ -1,35 +1,35 @@
-window.addEventListener('load',function(e){
+  // Configuracion de los Mensajes SweetAlert
+  function mensajeSweetAlert(icon, title, text, timer){
+    Swal.fire({
+      position: 'center',
+      icon: icon,
+      width:'300px',
+      heightAuto:false,
+      title: title,
+      text: text,
+      showConfirmButton: false,
+      timer: timer
+    })
+  }
 
+  // capturo el formulario
   const boton = document.getElementById('formulario');
 
+  // Determino acciones al enviar datos
   boton.addEventListener('submit', function(e){
-    e.preventDefault();
+    // Evito el envio instantaneo y que no se muestre el mensaje
+    e.preventDefault(); 
+
+    // Capturo los valores Ingresados al Formulario
     const nombre = document.getElementById('nombre').value;
     const apellido = document.getElementById('apellido').value;
     const edad = document.getElementById('edad').value;
 
+    // Valido que no hayan campos vacios
     if(nombre === '' || apellido ==='' || edad === '' ){
-      Swal.fire({
-        position: 'center',
-        icon: 'error',
-        width:'300px',
-        heightAuto:false,
-        title: 'Faltan datos',
-        showConfirmButton: false,
-        timer: 2000
-      })
-    }else{
-      Swal.fire({
-        position: 'center',
-        icon: 'success',
-        width:'300px',
-        heightAuto:false,
-        title: `${nombre} ${apellido}`,
-        text:`Tienes ${edad} Años de Edad`,
-        showConfirmButton: false,
-        timer: 3000
-      })
+      mensajeSweetAlert('error', 'Faltan Datos!', 'Llena Todos Los Campos!' , 2000);
+    }else{ //si no hay campos vacios, realiza el envio
+      mensajeSweetAlert('success', `${nombre} ${apellido}`, `Tienes ${edad} Años de Edad` , 2000);
       formulario.reset();
     }
   });
-})
